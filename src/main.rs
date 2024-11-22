7f447f0ea2ca03975803e53de9a1cb73e6e0b96f
human-d3v <brytop05@gmail.com>
2024-11-21 20:14:59 -0700

    Loaded data for model

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
-pub mod data_utils;
-pub mod error;
+use std::path::PathBuf;
 
-fn main() {
-    println!("Hello, world!");
+pub mod data_utils;
+use data_utils::{BostonRecord, DataLoader};
+
+pub mod error;
+use error::Result;
+
+fn main() -> Result<()>{
+    let mut p:PathBuf = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")
+        .expect("Unable to find crate root")
+    );
+    p.push("src/data/boston.csv");
+
+    let data = BostonRecord::from_csv(&p)
+        .expect("Error loading data from csv file");
+
+    println!("{:?}", data); 
+    Ok(())
 }
